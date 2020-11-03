d398d9aa45d1f23612392c4c398dca1c5167e597
Anton <aner-anton@ya.ru>
2020-11-03 19:03:07 +0300

    First commit

---
+++ parenthesis_checker.php
@@ -0,0 +1,23 @@
+<?php
+
+class ParenthesisChecker {
+  private $counter;
+
+  public function Check(string $str)
+  {
+    $this->counter = 0;
+    if (preg_match("/^[\(\)[:blank:]\r\n]+$/", $str, $matches) === 1 && 
+        strlen($matches[0]) === strlen($str)) {
+      $arr = str_split($str);
+      
+      foreach ($arr as $v) {
+        if ($v === '(') $this->counter++;
+        if ($v === ')') $this->counter--;        
+      }
+
+      if ($this->counter == 0) return true; 
+      else return false;
+    } else throw new InvalidArgumentException(
+      'Argument may contain only "(", ")", " ", "\t", "\r", "\n"');
+  }
+}
